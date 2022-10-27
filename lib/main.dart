f56c9b3b6a393e3ce8a26006eeaf89e582794589
TranPhamNhuThaoB1909987 <thaob1909987@student.ctu.edu.vn>
2022-10-27 15:16:12 +0700

    Cai dat chung thuc voi Firebase Authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:myshop/ui/cart/cart_manager.dart';
 import 'package:myshop/ui/orders/orders_manager.dart';
@@ -11,8 +10,12 @@ import 'ui/cart/cart_screen.dart';
 import 'ui/orders/orders_screen.dart';
 import 'package:provider/provider.dart';
 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'ui/screens.dart';
 
-void main() {
+
+Future <void> main() async {
+  await dotenv.load();
   runApp(const MyApp());
 }
 
@@ -25,6 +28,9 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
+          create: (context) => AuthManager(),
+        ),
+        ChangeNotifierProvider(
           create: (ctx) => ProductsManager(),
         ),
         ChangeNotifierProvider(
@@ -34,54 +40,66 @@ class MyApp extends StatelessWidget {
           create: (ctx) => OrdersManager(),
         ),
       ],
-      child : MaterialApp(
-        title: 'My Shop',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          fontFamily: 'Lato',
-          colorScheme: ColorScheme.fromSwatch(
-            primarySwatch: Colors.purple,
-          ).copyWith(
-            secondary: Colors.deepOrange,
-          ),
-        ),
-          home: const ProductsOverviewScreen(),
-          routes: {
-              CartScreen.routeName:
-                (ctx) => const CartScreen(),
-              OrdersScreen.routeName:
-                (ctx) => const OrdersScreen(),
-              UserProductsScreen.routeName:
-                (ctx) => const UserProductsScreen(),
-          },
-          onGenerateRoute: (settings) {
-            if (settings.name == ProductDetailScreen.routeName) {
-                final productId = settings.arguments as String;
-                return MaterialPageRoute(
-                  builder: (ctx) {
-                    return ProductDetailScreen(
-                      ctx.read<ProductsManager>().findById(productId)
-                    );
+      child : Consumer<AuthManager>(
+        builder: (ctx, authManager, child) {
+          return MaterialApp(
+            title: 'My Shop',
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              fontFamily: 'Lato',
+              colorScheme: ColorScheme.fromSwatch(
+                primarySwatch: Colors.purple,
+              ).copyWith(
+                secondary: Colors.deepOrange,
+              ),
+            ),
+              home: authManager.isAuth 
+                ? ProductsOverviewScreen()
+                : FutureBuilder(
+                  future: authManager.tryAutoLogin(),
+                  builder: (context, snapshot) {
+                    return snapshot.connectionState == ConnectionState.waiting
+                            ? const SplashScreen()
+                            : const AuthScreen();
                   },
-                );
-              }
+                ),
+              routes: {
+                  CartScreen.routeName:
+                    (ctx) => const CartScreen(),
+                  OrdersScreen.routeName:
+                    (ctx) => const OrdersScreen(),
+                  UserProductsScreen.routeName:
+                    (ctx) => const UserProductsScreen(),
+              },
+              onGenerateRoute: (settings) {
+                if (settings.name == ProductDetailScreen.routeName) {
+                    final productId = settings.arguments as String;
+                    return MaterialPageRoute(
+                      builder: (ctx) {
+                        return ProductDetailScreen(
+                          ctx.read<ProductsManager>().findById(productId)
+                        );
+                      },
+                    );
+                  }
 
-            if (settings.name == EditProductScreen.routeName) {
-                final productId = settings.arguments as String?;
-                return MaterialPageRoute(
-                  builder: (ctx) {
-                    return EditProductScreen(
-                      productId != null
-                      ? ctx.read<ProductsManager>().findById(productId)
-                      :null ,
+                if (settings.name == EditProductScreen.routeName) {
+                    final productId = settings.arguments as String?;
+                    return MaterialPageRoute(
+                      builder: (ctx) {
+                        return EditProductScreen(
+                          productId != null
+                          ? ctx.read<ProductsManager>().findById(productId)
+                          :null ,
+                        );
+                      },
                     );
-                  },
-                );
-              }
-              return null;
-          },  
-        ),
+                  }
+                  return null;
+              },  
+            );
+        }
+      ),
     );
   }
 }
-
