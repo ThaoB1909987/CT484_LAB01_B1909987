1f7a2da6b31b5c0bb43df4581abfc63e89b5a4d6
TranPhamNhuThaoB1909987 <thaob1909987@student.ctu.edu.vn>
2022-10-29 13:50:37 +0700

    Cap nhat, xoa san pham tren Realtime Database cua Firebase

--- lib/ui/products/products_manager.dart
+++ lib/ui/products/products_manager.dart
@@ -38,22 +38,43 @@ class ProductsManager with ChangeNotifier{
       return _items.firstWhere((prod) => prod.id == id);
     }
 
-    void updateProduct(Product product){
-      final index = _items.indexWhere((item) => item.id == product.id);
-      if(index >=0){
-        _items[index] = product;
-        notifyListeners();
-      }
-    }
+    // void updateProduct(Product product){
+    //   final index = _items.indexWhere((item) => item.id == product.id);
+    //   if(index >=0){
+    //     _items[index] = product;
+    //     notifyListeners();
+    //   }
+    // }
 
     void toggleFavoriteStatus(Product product){
       final savedStatus = product.isFavorite;
       product.isFavorite = !savedStatus;
     }
 
-    void deleteProduct(String id){
+    // void deleteProduct(String id){
+    //   final index = _items.indexWhere((item) => item.id == id);
+    //   _items.removeAt(index);
+    //   notifyListeners();
+    // }
+    Future<void> updateProduct(Product product) async{
+      final index = _items.indexWhere((item) => item.id == product.id);
+      if(index >=0){
+        if(await _productsService.updateProduct(product)){
+          _items[index] = product;
+          notifyListeners();
+        }
+      }
+    }
+
+    Future<void> deleteProduct(String id)async{
       final index = _items.indexWhere((item) => item.id == id);
+      Product? existingProduct = _items[index];
       _items.removeAt(index);
       notifyListeners();
+
+      if(!await _productsService.deleteProduct(id)){
+        _items.insert(index, existingProduct);
+        notifyListeners();
+      }
     }
 }
